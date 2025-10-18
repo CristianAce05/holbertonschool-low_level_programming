07c7c28f5fb0f9fc9843e36d10d09dd4deb12d84
Cristian Acevedo <acecristian35@gmail.com>
2025-10-17 21:55:10 -0400

    p9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			product = i * j;
-			
+
 			if (j == 0)
 			{
 				_putchar('0');
@@ -24,7 +24,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				
+
 				if (product < 10)
 				{
 					_putchar(' ');
