bd9dcce03be292741364726171dc918b8de252c6
Cristian Acevedo <acecristian35@gmail.com>
2025-10-23 13:37:41 -0400

    m1

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,5 +24,7 @@ void print_array(int *a, int n);
 int _strlen(char *s);
 int _putchar(char c);
 char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
