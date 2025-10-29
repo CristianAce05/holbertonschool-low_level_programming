364d1c1537a6171203a4329f7500f18d8864b9ae
Cristian Acevedo <acecristian35@gmail.com>
2025-10-29 10:16:45 -0400

    p0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: pointer to the string to print
+ */
+void _puts_recursion(char *s)
+{
+    if (*s == '\0')  /* Base case: end of string */
+    {
+        _putchar('\n');
+        return;
+    }
+
+    _putchar(*s);          /* Print current character */
+    _puts_recursion(s + 1); /* Recurse with the next character */
+}
