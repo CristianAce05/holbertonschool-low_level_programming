9cc0f311ec20d81720a4f77e22e453737e5038ca
Cristian Acevedo <acecristian35@gmail.com>
2025-10-30 10:27:53 -0400

    p3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,25 +11,25 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
+	int num1, num2, result;
 
-    /* Check if exactly two arguments are provided (excluding program name) */
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	/* Check if exactly two arguments are provided (excluding program name) */
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    /* Convert arguments from strings to integers */
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
+	/* Convert arguments from strings to integers */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-    /* Perform multiplication */
-    result = num1 * num2;
+	/* Perform multiplication */
+	result = num1 * num2;
 
-    /* Print the result followed by a new line */
-    printf("%d\n", result);
+	/* Print the result followed by a new line */
+	printf("%d\n", result);
 
-    /* Return 0 to indicate successful execution */
-    return (0);
+	/* Return 0 to indicate successful execution */
+	return (0);
 }
