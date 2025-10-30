c3ae1c55ecfc5a141eb9c557da037e3e286314ba
Cristian Acevedo <acecristian35@gmail.com>
2025-10-30 10:37:50 -0400

    p5

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -2,54 +2,40 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change for an amount
+ * main - prints the minimum number of coins to make change
+ * for an amount of money
  *
  * @argc: argument count
- * @argv: array of argument strings
+ * @argv: argument vector
  *
  * Return: 0 if successful, 1 if error
  */
 int main(int argc, char *argv[])
 {
 	int cents, coins = 0;
+	int coin_values[] = {25, 10, 5, 2, 1};
+	int i;
 
-	/* Check if exactly one argument is provided */
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	/* Convert argument to integer */
 	cents = atoi(argv[1]);
 
-	/* If number is negative, print 0 */
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	/* Calculate minimum coins using greedy algorithm */
-	while (cents > 0)
+	for (i = 0; i < 5; i++)
 	{
-		if (cents >= 25)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else
-			cents -= 1;
-
-		coins++;
+		coins += cents / coin_values[i];
+		cents %= coin_values[i];
 	}
 
-	/* Print total number of coins */
 	printf("%d\n", coins);
-
-	/* Return 0 to indicate successful execution */
 	return (0);
 }
