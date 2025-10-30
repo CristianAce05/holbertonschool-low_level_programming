1b0f35a734566d1ae8f1be4a9136ec825180d68e
Cristian Acevedo <acecristian35@gmail.com>
2025-10-30 10:15:29 -0400

    p0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints its own name (including path), followed by a new line
- * Return: Always 0
+ * main - prints its own name (including the path), followed by a new line
+ *
+ * @argc: argument count (not used)
+ * @argv: array of strings containing command-line arguments
+ *
+ * Return: Always 0 (success)
  */
 int main(int argc, char *argv[])
 {
-	(void)argc; /* Unused parameter */
+	(void)argc; /* Suppress unused variable warning for argc */
+
+	/* Print the name of the program (including its path) */
 	printf("%s\n", argv[0]);
+
+	/* Return 0 to indicate successful execution */
 	return (0);
 }
