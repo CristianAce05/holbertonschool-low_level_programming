0720f7ef2f500c413eb18a30eaf767bbe3366605
Cristian Acevedo <acecristian35@gmail.com>
2025-11-11 10:52:51 -0500

    p0

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,29 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <wctype.h>
+#include <stddef.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
