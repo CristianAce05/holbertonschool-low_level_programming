99c03af9289f3338460d71696cd112a7823cc0d2
Cristian Acevedo <acecristian35@gmail.com>
2025-11-26 12:14:16 -0500

    p1

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -3,20 +3,6 @@
 
 #include <stddef.h>
 
-/* Singly linked list */
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
-
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
 /* Doubly linked list */
 typedef struct dlistint_s
 {
@@ -25,7 +11,10 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+/* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif /* LISTS_H */
+
 
