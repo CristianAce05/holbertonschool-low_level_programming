87b3f2f98794a0fa1964eb47665f72ae79e2c3ff
Cristian Acevedo <acecristian35@gmail.com>
2025-11-30 10:48:53 -0500

    p0

---
+++ file_io/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <wctype.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
